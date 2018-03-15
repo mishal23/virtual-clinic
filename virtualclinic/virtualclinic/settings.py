a59c6f54f43ddaec855ca3d8135e002c590759c7
Mishal Shah <shahmishal1998@gmail.com>
2018-03-15 22:45:53 +0530

    MYSQL database done

--- virtualclinic/virtualclinic/settings.py
+++ virtualclinic/virtualclinic/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'virtualclinic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'virtual_clinic',
+        'USER': 'admin',
+        'PASSWORD': 'Admin@vc1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
