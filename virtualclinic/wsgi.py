57ef046e1fe898e3db8e1fb274f4befc3e9fd7ff
Mishal Shah <shahmishal1998@gmail.com>
2018-09-08 02:12:54 +0530

    moved code to root directory

---
+++ virtualclinic/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for virtualclinic project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "virtualclinic.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
