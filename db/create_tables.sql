391ca689ab2220263c1c949002321b989cf4c2ec
Mishal Shah <shahmishal1998@gmail.com>
2018-09-08 13:24:37 +0530

    create tables queries added, still buggy

---
+++ db/create_tables.sql
@@ -0,0 +1,118 @@
+CREATE DATABASE IF NOT EXISTS `vclinic`;
+use `vclinic`;
+
+CREATE TABLE IF NOT EXISTS `speciality` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`description` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `symptom` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`description` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `location` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`city` varchar(50) NOT NULL,
+	`zip` varchar(50) NOT NULL,
+	`state` varchar(50) NOT NULL,
+	`country` varchar(50) DEFAULT 'India',
+	`address` varchar(50) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `hospital` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`phone` varchar(10) NOT NULL,
+	`location_id` int UNIQUE, 
+	PRIMARY KEY (`id`),
+	CONSTRAINT FOREIGN KEY (`location_id`) REFERENCES `location`(`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `account` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`role` ENUM ('Patient','Doctor','Admin','Lab','Chemist'),
+	`profile_id` int UNIQUE,
+	`user_id` int UNIQUE,
+	`archived` bool DEFAULT False,
+	PRIMARY KEY (`id`)
+	-- CONSTRAINT FOREIGN KEY(`profile_id`) REFERENCES `profile`(`id`) ON DELETE CASCADE
+	-- CONSTRAINT FOREIGN KEY(`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `profile` (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`firstname` varchar(50),
+	`lastname` varchar(50),
+	`sex` ENUM ('M', 'F'),
+	`birthday` timestamp DEFAULT CURRENT_TIMESTAMP,
+	`phone` varchar(10),
+	`allergies` varchar(250),
+	`created` timestamp,
+	`prefHospital_id` int UNIQUE,
+	`primaryCareDoctor_id` int UNIQUE,
+	PRIMARY KEY (`id`),
+	CONSTRAINT FOREIGN KEY(`prefHospital_id`) REFERENCES `hospital`(`id`) ON DELETE CASCADE
+	-- CONSTRAINT FOREIGN KEY(`primaryCareDoctor_id`) REFERENCES `account`(`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `appointment` (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`doctor_id` int NOT NULL UNIQUE,
+	`patient_id` int NOT NULL UNIQUE,
+	`description` varchar(250) NOT NULL,
+	`status` varchar(50) NOT NULL DEFAULT 'Active',
+	`hospital_id` int UNIQUE,
+	`appointment_type` varchar(10) DEFAULT 'Offline',
+	`startTime` timestamp NOT NULL,
+	`endTime` timestamp NOT NULL,
+	PRIMARY KEY (`id`),
+	CONSTRAINT FOREIGN KEY(`doctor_id`) REFERENCES `account`(`id`) ON DELETE CASCADE,
+	CONSTRAINT FOREIGN KEY(`patient_id`) REFERENCES `account`(`id`) ON DELETE CASCADE,
+	CONSTRAINT FOREIGN KEY(`hospital_id`) REFERENCES `hospital`(`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `message` (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`target_id` int UNIQUE,
+	`sender_id` int UNIQUE,
+	`header` varchar(100) NOT NULL,
+	`body` varchar(1000) NOT NULL,
+	`timestamp` timestamp NOT NULL ,
+	PRIMARY KEY (`id`),
+	CONSTRAINT FOREIGN KEY(`target_id`) REFERENCES `account`(`id`) ON DELETE CASCADE,
+	CONSTRAINT FOREIGN KEY(`sender_id`) REFERENCES `account`(`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `prescription` (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`patient_id` int UNIQUE,
+	`doctor_id` int UNIQUE,
+	`date` timestamp NOT NULL,
+	`medication` varchar(100) NOT NULL,
+	`instruction` varchar(100) NOT NULL,
+	`active` bool DEFAULT 'True',
+	PRIMARY KEY (`id`),
+	CONSTRAINT FOREIGN KEY(`patient_id`) REFERENCES `account`(`id`) ON DELETE CASCADE,
+	CONSTRAINT FOREIGN KEY(`doctor_id`) REFERENCES `account`(`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `medical_test` (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`name` varchar(100) NOT NULL,
+	`hospital_id` int UNIQUE,
+	`description` varchar(100) NOT NULL,
+	`doctor_id` int UNIQUE,
+	`patient_id` int UNIQUE,
+	`date` timestamp NOT NULL,
+	`completed` bool DEFAULT 'False',
+	PRIMARY KEY (`id`),
+	CONSTRAINT FOREIGN KEY(`patient_id`) REFERENCES `account`(`id`) ON DELETE CASCADE,
+	CONSTRAINT FOREIGN KEY(`doctor_id`) REFERENCES `account`(`id`) ON DELETE CASCADE
+);
+
